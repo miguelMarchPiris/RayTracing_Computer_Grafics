ceb8b24eb572b0878de37699b131f6d8d55bac2f
marcosPlaza <marcos.plaza.gonzalez@gmail.com>
2020-03-28 13:27:54 +0100

    creacio branca triangle

---
+++ include/Triangle.h
@@ -0,0 +1,14 @@
+//
+// Created by marcos on 8/3/20.
+//
+
+#ifndef RAYDATA_TRIANGLE_H
+#define RAYDATA_TRIANGLE_H
+
+
+class Triangle {
+
+};
+
+
+#endif //RAYDATA_TRIANGLE_H
---
+++ src/Triangle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marcos on 8/3/20.
+//
+
+#include "Triangle.h"
