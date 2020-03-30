ceac6ed8e0bbf95d7f513af3316b5dc79b35340c
Joan Trave <jtravegordillo@gmail.com>
2020-03-30 18:34:58 +0200

    Removed cmake build debug

--- cmake-build-debug/CMakeFiles/RayData_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/RayData_autogen"
-  "RayData_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RayData_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
