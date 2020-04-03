616941a0f0322c807a0a23862fca46878cbbb0d1
Miguel <mmarchpi7@alumnes.ub.edu>
2020-04-03 08:17:53 +0200

    Código en Transparent limpio. Refracciones y reflexiones funcionando a la perfección.

--- src/SceneFactoryVirtual.cpp
+++ src/SceneFactoryVirtual.cpp
@@ -35,21 +35,21 @@ void SceneFactoryVirtual::OneSphere(Scene *s) {
     Sphere * sphere_base = new Sphere(vec3(0, -100.5, -1), 100, 1.0);
     sphere_base->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(0.8, 0.8, 0.0), vec3(1, 1, 1), 1, 10));
 
-    Sphere * sphere2 = new Sphere(vec3(-2, 1, 1), 1, 1.0);
-    sphere2->setMaterial(new Metal(vec3(0.2, 0.2, 0.2), vec3(0.7, 0.7, 0.7), vec3(0.7, 0.7, 0.7), 1, 10));
+    Sphere * esfera_metalica = new Sphere(vec3(-0.6, 1.4, -5.2), 1.2, 1.0);
+    Cylinder*cilindro_transparente               = new Cylinder(vec3(1., 0.5, -2.0), 1.5, 2.5, 1.0);
+    Sphere * esfera_transparente  = new Sphere(vec3(0.0, 1.6, -2.0), 1.5, 1.0);
+    Sphere * esfera_blanca            = new Sphere(vec3(1.75, 1.5, -5.0), 0.6, 1.0);
+    Sphere * esfera_fucsia             = new Sphere(vec3(2.5, 1.5, -5.5), 0.8, 1.0);
+    Sphere * esfera_verde            = new Sphere(vec3(-2.0, 3.5, -3.0), 1.3, 1.0);
+
+    esfera_metalica->setMaterial(new Metal(vec3(0.2, 0.2, 0.2), vec3(0.7, 0.7, 0.7), vec3(0.7, 0.7, 0.7), 1, 10));
+    cilindro_transparente              ->setMaterial(new Transparent(vec3(1, 1, 1) * 0.9f, 1.35));
+    esfera_transparente->setMaterial(new Transparent(vec3(1, 1, 1) * 0.9f, 1.05));
+    esfera_blanca->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(1, 1, 1), vec3(1, 1, 1), 1, 10));
+    esfera_fucsia->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(0.8, 0.0, 0.6), vec3(1, 1, 1), 1, 10));
+    esfera_verde->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(0, 1, 0), vec3(1, 0, 1), 1, 10));
 
 
-    Sphere * sphere_transparent = new Sphere(vec3(0.0, 1.6, -2.0), 1.5, 1.0);
-    Sphere * sphere0            = new Sphere(vec3(0.5, 0.5, 0.0),0.6, 1.0);
-    Sphere * sphaux             = new Sphere(vec3( 2.0, 1.5, -5.5),0.8,1.0);
-    Sphere * sphere4            = new Sphere(vec3( -2.0, 1.0, -6.0),1.3,1.0);
-
-    sphere_transparent->setMaterial(new Transparent(vec3(1, 1, 1), 1.05));
-    sphere0->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(0.5, 0.5, 0.5), vec3(1, 1, 1), 1, 10));
-    sphere4->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(0.8, 0.0, 0.6), vec3(1, 1, 1), 1, 10));
-    sphaux->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(0,1,0), vec3(1, 0, 1), 1, 10));
-    Cylinder *cil0 = new Cylinder(vec3(0, 0.5f, 0), 1, 1, 1.0);
-    cil0->setMaterial(new Transparent(vec3(1, 1, 1), 1.05));
     BoundaryObject *bro0 = new BoundaryObject("../resources/cube.obj", 1.0);
     bro0->setMaterial(new Lambertian(vec3(0.2, 0.2, 0.2), vec3(0.5, 0.5, 0.5), vec3(1, 1, 1), 1, 10));
 
@@ -61,13 +61,13 @@ void SceneFactoryVirtual::OneSphere(Scene *s) {
     // anim->transf = new Translate(vec3(0.2));
     // sphere->addAnimation(anim);
 
-    s->objects.push_back(sphere0);
+    s->objects.push_back(esfera_blanca);
     s->objects.push_back(sphere_base);
-    //s->objects.push_back(sphere2);
-    s->objects.push_back(sphere_transparent);
-    s->objects.push_back(sphere4);
-    s->objects.push_back(sphaux);
-    //s->objects.push_back(cil0);
+    s->objects.push_back(esfera_metalica);
+    //s->objects.push_back(esfera_transparente);
+    s->objects.push_back(esfera_verde);
+    s->objects.push_back(esfera_fucsia);
+    s->objects.push_back(cilindro_transparente);
 
     s->lights.push_back(light0);
     //s->lights.push_back(light1);
@@ -78,7 +78,7 @@ Camera *SceneFactoryVirtual::createCamera() {
     // creacio de la camera
     vec3 lookfrom(0, 5.5, 13);
     //vec3 lookfrom(-10, 2, 13);
-    vec3 lookat(0,1.5,0);
+    vec3 lookat(0,2.1,0);
     float dist_to_focus = length(lookfrom-lookat);
     float aperture = 0.1;
     int pixelsX = 600;
--- src/Transparent.cpp
+++ src/Transparent.cpp
@@ -7,20 +7,13 @@
 
 Transparent::Transparent(vec3 kt, float refractionIndex) : Material()
 {
-    /*
-    Kd = vec3(1.0), Ks = vec3(1.0),
-Kt = vec3(0.7), index = 1.5
-     */
-    //
-    this->specular=vec3(1,1,1)*0.01f;//.9351f;
-    this->kt= vec3(1,1,1)-specular;
-    //this->kt= vec3(1,1,1)*0.9f;
+    this->kt=kt;
+    this->specular=vec3(1,1,1)-this->kt;
     this->diffuse=vec3(0,0,0);
     this->ambient=vec3(0,0,0);
-    //this->kt = kt;
+
     this->refractionIndex = refractionIndex;
-    this->refractionIndex=1.297;
-    this->shininess=400;
+    this->shininess=500;
 }
 
 Transparent::~Transparent(){
@@ -36,55 +29,34 @@ bool Transparent::scatter(const Ray& r_in, const IntersectionInfo& rec, std::vec
     //INVERTIMOS el vector director.
     vec3 in_inv = normalize(r_in.dirVector())*-1.f;
     vec3 in     = normalize(r_in.dirVector());
-
     in_cos = dot(in_inv, normal);
-
-    //nit=1/refractionIndex;
-
-    //Si angleIn es menor que 90 significa que pasamos del "vacío" al material en cuestión
-    // como anglein es el coseno del angulo, tenemos anglein>0 <-> angulo<90
     nit = 1/refractionIndex;
-    if(in_cos>0){
-        //nit = 1/refractionIndex;
-    }
-    //Sino significa que pasamos del material al "vació" -> refractionIndex/1
-    else{
+
+    //Si angleIn es mayor que 90 significa que pasamos del material al "vació"
+    //por lo tanto invertimos la normal.
+    if(in_cos<0){
         normal=normal*-1.f;
-        //nit = refractionIndex;
     }
+
     dentro_raiz = 1 - nit * nit * (1 - in_cos * in_cos);
-    // Si es menor que 0, solo tenemos en cuenta la reflexión total interna.
+    // Si dentro_raiz es menor que 0, solo tenemos en cuenta la reflexión total interna.
     bool refraccion_total=true;
     if(dentro_raiz > 0){
-        float ec1 = (nit * in_cos - sqrt(dentro_raiz));
-        float ec2 = nit;
-        //vec3 transmissionVector = (in_inv * nit) + normal*(nit * in_cos - sqrt(dentro_raiz));
-        vec3 transmissionVector = (in * ec2) + normal * ec1;
-        //vec3 transmissionVector = r_in.dirVector();
+        vec3 transmissionVector = (in * nit) + normal*(nit * in_cos - sqrt(dentro_raiz));
         Ray transmissionRay = Ray(rec.p + 0.001f*transmissionVector, transmissionVector);
         r_out.push_back(transmissionRay);
-        //colors.push_back(vec3(0,0,0));
         colors.push_back(this->kt);
+
         refraccion_total=false;
     }
-
-    //else{
-
-        vec3 reflectedVector = normal*2.f + rec.p - in_inv;
-        Ray reflectedRay = Ray(rec.p + 0.001f*reflectedVector, reflectedVector);
-        r_out.push_back(reflectedRay);
-
-        //colors.push_back(vec3(0,0,0));
-        if(refraccion_total){
-            colors.push_back(vec3(1,1,1));
-        }else{
-            colors.push_back(this->specular);
-        }
-
-
-    //}
-    // */
-
-
+    vec3 reflectedVector = normal*2.f + rec.p - in_inv;
+    Ray reflectedRay = Ray(rec.p + 0.001f*reflectedVector, reflectedVector);
+    r_out.push_back(reflectedRay);
+    if(refraccion_total){
+        //En caso de refracción total el valor del color es máximo
+        colors.push_back(vec3(1,1,1));
+    }else{
+        colors.push_back(this->specular);
+    }
     return true;
 }
